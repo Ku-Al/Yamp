e65e8e55a600e4ed7ef72161a4482013e1db7ac0
Ku-Al <kubov.aleksandr@gmail.com>
2013-09-19 09:45:19 +0300

    Open new tab

--- popup.js
+++ popup.js
@@ -3,8 +3,9 @@ function openw(){
 	chrome.tabs.create({url: 'http://ya.ru'});
 }
 
-var q = console;
-window.close();
-q.log("work level 0");
-openw;
-window.open('http://ya.ru');
+//var q = console;
+//window.close();
+//q.log("work level 0");
+console.log("start");
+openw();
+//window.open('http://ya.ru');
